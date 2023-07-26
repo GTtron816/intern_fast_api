81e97512a84add61a003ecdb51a4d35a5c39bd51
GTtron816 <abinak322@gmail.com>
2023-07-26 15:45:05 +0530

    -changes

--- mainorm.py
+++ mainorm.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
+from typing import List
 import  models
 import schema
 
@@ -25,27 +26,32 @@ app.add_middleware(
 )
 
 
-@app.get('/')
+
+@app.get('/',response_model=List[schema.Response])
 def get_user(db: Session = Depends(get_db)):
     characters=db.query(models.Characater).all()
     return characters
     
   
 
-@app.get('/{id}')
+@app.get('/{id}',response_model=List[schema.Response])
 def get_user(id:int,db: Session = Depends(get_db)):
     characters=db.query(models.Characater).filter(models.Characater.cid == id).first()
     return [characters]
  
         
-@app.post('/')
+@app.post('/',response_model=schema.PostResponse)
 def add_item(item:schema.Character,db: Session = Depends(get_db)):
     new_character=models.Characater(**item.dict())
     db.add(new_character)
     db.commit()
+    return new_character
 
-@app.put('/{id}')
+@app.put('/{id}',response_model=schema.PostResponse)
 def update_item(id:int,item:schema.Character,db: Session = Depends(get_db)):
-    db.query(models.Characater).filter(models.Characater.cid == id).update({**item.dict()})
+    query=db.query(models.Characater).filter(models.Characater.cid == id)
+    query.update({**item.dict()})
     db.commit()
+    ret=query.first()
+    return ret
      
--- schema.py
+++ schema.py
@@ -4,4 +4,19 @@ class Character(BaseModel):
     species:str
     year:int
     image:str
+class Response(BaseModel):
+    cid:int
+    name:str
+    species:str
+    year:int
+    image:str
+    class Config:
+        orm_mode=True
+class PostResponse(BaseModel):
+    cid:int
+    name:str
+    class Config:
+        orm_mode=True
+   
+    
     
